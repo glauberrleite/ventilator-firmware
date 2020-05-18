f4881f7253cc666676135537311a7641cc818b55
vangasse <vangasse@ic.ufal.br>
2020-05-17 22:10:37 -0300

    inicio de modularização de sensores

---
+++ src/sensors.h
@@ -0,0 +1,10 @@
+#include <math.h>
+// Sensor functions
+
+float getFlowAWM720P(float v);
+
+float getPressureASDX(float v, float p_min, float p_max);
+
+float getPressureASDX001PDAA5(float v);
+
+float getPressureASDX005NDAA5(float v);

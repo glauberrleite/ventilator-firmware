bf2797fa8d29407725ed2605254a5863f138b921
vangasse <vangasse@ic.ufal.br>
2020-05-28 20:05:54 -0300

    treating repeated terms in x-axis

--- test/fitting_routine.m
+++ test/fitting_routine.m
@@ -2,6 +2,18 @@ readings = readtable('list.csv', 'HeaderLines', 1)
 dp = table2array(readings(:,3))
 flow = table2array(readings(:,1))
 %Certifique-se que dp não possui sinais de entrada iguais
+
+for i = 1:1:length(dp)
+    for j = i+1:1:length(dp)
+        if dp(i)==dp(j)
+            flow(i)=(flow(i)+flow(j))/2
+            flow(j)=[]
+            dp(j)=[]
+            j=j-1
+        end
+    end
+end
+
 pp = spline(dp,flow)
 xx = -1:.1:1;
 plot(dp, flow)
